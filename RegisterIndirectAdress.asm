a427de8af04a244ce2ffe707d4b89577a61145f3
Esra Budak <esra_b1644@outlook.com>
2021-12-01 16:06:48 +0300

    Register Indirect Adressing
    Addressing modes and memory
    operations in 8086 emulator software.

---
+++ RegisterIndirectAdress.asm
@@ -0,0 +1,30 @@
+
+; You may customize this and other start-up templates; 
+; The location of this template is c:\emu8086\inc\0_com_template.txt
+
+MOV AX , 0200H
+
+MOV DS , AX
+
+MOV DI , 0000H
+MOV [DI] ,01H
+
+MOV DI , 0001H
+MOV [DI] , 02H 
+
+MOV DI , 0002H
+MOV [DI] ,03H 
+
+MOV DI , 0003H
+MOV [DI] ,04H
+
+
+MOV DI , 0004H
+MOV [DI] , 05H  
+
+MOV CX , 5
+
+ret
+
+
+
